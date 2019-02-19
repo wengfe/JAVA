563f761ecd553c441ef94ee06daf5d1bb0860e98
wengfuyi <214952026@qq.com>
2019-02-19 16:49:53 +0800

    Writer/Reader

--- Throwable_example/src/fileOutPutStream/File_Stream.java
+++ Throwable_example/src/fileOutPutStream/File_Stream.java
@@ -23,6 +23,25 @@ public class File_Stream {
                             new FileInputStream("b.dat")));
             int j = in.readInt();
             System.out.println(j);
+
+//            Reader/Writer
+            PrintWriter out_p = new PrintWriter(
+                    new BufferedWriter(
+//                            OutputStreamWriter 构建了 Stream 和 Writer 之间的桥梁
+                            new OutputStreamWriter(
+                                    new FileOutputStream("a.txt"))));
+            int k = 123456;
+//            文本流中的 print 表示打印到文件中？
+            out_p.print(k);
+            out_p.close();
+//            读文件
+            BufferedReader in_r = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream("src/../a.txt")));
+            String line;
+            while ((line=in_r.readLine()) != null){
+                System.out.println(line);
+            }
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
