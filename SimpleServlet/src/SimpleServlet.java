d0a75731e1a46e33184080ef3821854cf92d31be
wengfuyi <214952026@qq.com>
2019-02-21 16:12:02 +0800

    servlet 的简单实现

---
+++ SimpleServlet/src/SimpleServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SimpleServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("service method");
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGTE method");
+        PrintWriter pw = resp.getWriter();
+        pw.print("hello world");
+        pw.close();
+//        super.doGet(req, resp);
+    }
+
+
+}
