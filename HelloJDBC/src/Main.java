ece78de9a0b63941060819fd2531241cc3798f0e
wengfuyi <214952026@qq.com>
2019-03-22 15:31:23 +0800

    JDBC基础使用实例

---
+++ HelloJDBC/src/Main.java
@@ -0,0 +1,49 @@
+import java.sql.*;
+
+public class Main {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/test?useSSL=true";
+    static final String USER = "root";
+    static final String PASSWORD = "admin";
+
+    public static void main(String[] args) throws ClassNotFoundException{
+        Connection conn = null;
+        Statement statemente = null;
+        ResultSet rs = null;
+//      1. 装载数据库驱动程序；
+        Class.forName(JDBC_DRIVER);
+//      2. 通过JDBC建立数据库连接；
+        try {
+            conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+//      3.访问数据库，执行SQL语句；
+            statemente = conn.createStatement();
+            rs = statemente.executeQuery("select name from test.user ");
+//      4.获得执行结果
+            while(rs.next()) {
+                System.out.println("Hello " + rs.getString("name"));
+            }
+
+
+
+        } catch (SQLException e) {
+//            异常处理
+            e.printStackTrace();
+        } finally {
+            // 5. 断开数据库连接，清理环境
+            try {
+                if(conn != null) {
+                    conn.close();
+                }
+                if(statemente != null) {
+                    statemente.close();
+                }
+                if(rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                // pass
+            }
+
+        }
+    }
+}
