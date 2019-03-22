d06444d81793d2ac60ad857ebbe7e31d4829d562
wengfuyi <214952026@qq.com>
2019-03-22 16:28:11 +0800

    使用游标分批查询多条记录

--- HelloJDBC/src/Main.java
+++ HelloJDBC/src/Main.java
@@ -2,22 +2,28 @@ import java.sql.*;
 
 public class Main {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/test?useSSL=true";
+    static String DB_URL = "jdbc:mysql://localhost:3306/test?useSSL=true";
     static final String USER = "root";
     static final String PASSWORD = "admin";
 
     public static void main(String[] args) throws ClassNotFoundException{
         Connection conn = null;
         Statement statemente = null;
+        PreparedStatement preparedStatement = null;
         ResultSet rs = null;
 //      1. 装载数据库驱动程序；
         Class.forName(JDBC_DRIVER);
 //      2. 通过JDBC建立数据库连接；
         try {
+            //在 url 中 开启 游标功能
+            DB_URL = DB_URL + "&useCursorFetch=true";
             conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
 //      3.访问数据库，执行SQL语句；
-            statemente = conn.createStatement();
-            rs = statemente.executeQuery("select name from test.user ");
+//            statemente = conn.createStatement();
+//            rs = statemente.executeQuery("select name from test.user ");
+            preparedStatement = conn.prepareStatement("select  name from test.user");
+            preparedStatement.setFetchSize(1);
+            rs = preparedStatement.executeQuery();
 //      4.获得执行结果
             while(rs.next()) {
                 System.out.println("Hello " + rs.getString("name"));
